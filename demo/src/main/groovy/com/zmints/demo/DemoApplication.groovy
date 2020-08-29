ddbc90be9b8b71ae42459611fb70f57ec02bfc12
vzilberm <vzilbermints@gmail.com>
2020-08-29 16:04:35 -0500

    Viktoriya & Nakib: first springboot project

---
+++ demo/src/main/groovy/com/zmints/demo/DemoApplication.groovy
@@ -0,0 +1,13 @@
+package com.zmints.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DemoApplication, args)
+	}
+
+}
